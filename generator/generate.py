f6a8559478552496d19a2e53092b6d03348f0f0e
Max Adamski <maxadamski@gmx.com>
2019-12-12 07:07:48 +0100

    finished simulation

--- generator/generate.py
+++ generator/generate.py
@@ -1,3 +1,4 @@
+import time
 import numpy as np
 import pandas as pd
 import requests as req
@@ -6,16 +7,19 @@ from pprint import pprint
 from sys import stderr
 from google.protobuf.json_format import MessageToDict
 from math import radians, pi
+from numpy.random import beta, randint, uniform
 import pygeotile.tile
 
 import generator.proto.vector_tile_pb2
 
-req_codes = req.status_codes._codes
-
-
-API_KEY = open("generator/.tomtomkey").read().strip()
+REQ_CODES = req.status_codes._codes
+API_KEY_PATH = "generator/.tomtomkey"
+API_KEY = open(API_KEY_PATH).read().strip()
 API_ROOT = "api.tomtom.com"
 
+DECAY_PERIOD = 15*1_000 # seconds
+DECAY_PERIOD /= 5
+DECAY_LIMIT = 0.25
 
 def latlon2tile(lat, lon, zoom):
     x, y = pygeotile.tile.Tile.for_latitude_longitude(lat, lon, zoom).google
@@ -38,7 +42,6 @@ def pbf(Message, response):
     msg = Message()
     msg.ParseFromString(response.content)
     return msg
-    # return MessageToDict(msg)
 
 
 def tomtom_url(path, args):
@@ -59,25 +62,51 @@ def tomtom(path, debug=True, **args):
         print(f"GET {url}", file=stderr)
     if not res:
         code = str(res.status_code)
-        if code in req_codes:
-            code += f" ({req_codes[code][0]})"
+        if code in REQ_CODES:
+            code += f" ({REQ_CODES[code][0]})"
         print(f"ERR {code} -- {url_safe}", file=stderr)
         return None
     return res
 
 
+COLS = []
 def init_state(count=100):
-    lat, lon = 52.52343, 13.41144
+    lat, lon = 52.52343, 13.41144 # berlin
     X = pd.DataFrame()
-    X["lat"] = 0.1 * (np.random.beta(2, 2, size=count) - 0.5) + lat
-    X["lon"] = 0.1 * (np.random.beta(2, 2, size=count) - 0.5) + lon
-    X["state"] = np.random.randint(2, size=count)
+    X["lat"] = 0.3 * (beta(3, 3, size=count) - 0.5) + lat
+    X["lon"] = 0.6 * (beta(3, 3, size=count) - 0.5) + lon
+    X["decay"] = beta(3, 2, size=count)
+    X.loc[X.decay < DECAY_LIMIT, 'decay'] = 0
+    X["parked"] = X.decay >= DECAY_LIMIT
+    X["last_seen"] = time.time()
+    X["time_parked"] = 0
+    # cheats
+    global COLS
+    COLS = ['parked', 'decay', 'last_seen', 'time_parked']
+    COLS = [X.columns.get_loc(c) for c in COLS if c in X]
     return X
 
 
 def next_state(X):
+    # advance decay of parked cars to t
+    parked = X.parked == True
+    t = time.time() - X.loc[parked, 'time_parked']
+    X.loc[parked, 'decay'] = np.e**(-t/DECAY_PERIOD)
+    # mark timed out slots as free
+    timeout = X.decay < DECAY_LIMIT
+    X.loc[timeout, ['decay', 'parked', 'time_parked']] = 0
+    X.loc[timeout, 'parked'] = False
+    # park random cars
+    R = X[X.parked == False].sample(frac=0.05) # amount of cars
+    chances = uniform(0, 1, len(R))
+    R = R[chances <= 0.3] # probability of parking
+    X = did_park(X, R.index)
     return X
 
+def did_park(X, indices):
+    t = time.time()
+    X.iloc[indices, COLS] = (True, 1, t, t)
+    return X
 
 if __name__ == "__main__":
     # xml(tomtom('search/2/search/berline.xml'))
@@ -85,15 +114,19 @@ if __name__ == "__main__":
     # xml(tomtom('parkingprobabilities/v1'))
     # xml(tomtom('search/2/additionalData.json', geometries=<geometryIds>[&geometriesZoom=<zoomLevel>]
     # pprint(json(tomtom('search/2/geocode/berlin.xml', countrySet='DE', limit='1')))
-    # berlin = json(tomtom('search/2/search/berlin.json', limit=1, countrySet='DE'))
+    berlin = json(tomtom('search/2/search/berlin.json', limit=1, countrySet='DE'))
     # pprint(berlin)
 
-    # zoom = 7
-    # coord = berlin['results'][0]['position']
-    # x, y = coord2tile(coord, zoom)
+    zoom = 15
+    coord = berlin['results'][0]['position']
+    x, y = coord2tile(coord, zoom)
     # print(coord, x, y)
-    # map = tomtom(f'map/1/tile/basic/main/{zoom}/{x}/{y}.pbf')
-    # pprint(dir(val))
-    # pprint(pbf(proto.vector_tile_pb2.Tile, map))
+    map = tomtom(f'map/1/tile/basic/main/{zoom}/{x}/{y}.pbf')
+    vec = pbf(proto.vector_tile_pb2.Tile, map)
+    for layer in vec.layers:
+        if 'road' in layer.name.lower():
+            pprint(layer.name)
+            #pprint(layer)
     # print(init_state())
-    print(init_state())
+    #print(init_state())
+
---
+++ generator/server.py
@@ -0,0 +1,89 @@
+#!/usr/bin/env python3
+
+import os, sys, time, threading, traceback
+from sys import stderr, argv
+from pprint import pprint
+sys.path.append('.')
+
+from flask import Flask, jsonify, request, abort, make_response
+from flask_cors import CORS
+
+from generator.generate import init_state, next_state
+
+app = Flask(__name__)
+CORS(app)
+
+UPDATE_RATE = 2 # Hz
+
+sem = threading.Lock()
+sem.acquire()
+state = init_state(count=1_000)
+sem.release()
+
+def log(*args):
+    print(file=stderr, *args)
+
+def update_state(elapsed):
+    global state
+    sem.acquire()
+    state = next_state(state)
+    sem.release()
+
+def add_observation(lat, lon, parked1):
+    sem.acquire(timeout=1)
+    slot = (state.lat == lat) & (state.lon == lon)
+    if not any(slot):
+        log('ERR invalid lat and lon')
+        sem.release()
+        return
+    i = state[slot][0].index
+    T = time.time()
+    parked0 = state.loc[i, 'parked']
+    state.loc[i, 'last_seen'] = T
+    state.loc[i, 'parked'] = parked1
+    state.loc[i, 'decay'] = 1 if parked else 0
+    if parked0 == False and parked1 == True:
+        state.loc[i, 'time_parked'] = T
+    sem.release()
+
+def timer():
+    threading.Timer(UPDATE_RATE, timer).start()
+    update_state(UPDATE_RATE)
+
+@app.route('/map', methods=['GET'])
+def map():
+    return jsonify(state.to_dict(orient='list'))
+
+@app.route('/observe', methods=['POST'])
+def update():
+    if any(key not in request.args for key in ['lat', 'lon', 'parked']):
+        return make_response('bad parameters', 400)
+    lat, lon = float(request.args['lat']), float(request.args['lon'])
+    parked = bool(int(request.args['parked']))
+    add_observation(lat, lon, parked)
+    return make_response('ok', 200)
+
+@app.errorhandler(400)
+def bad_request(error):
+    return make_response('bad request', 400)
+
+@app.errorhandler(404)
+def bad_request(error):
+    return make_response('not found', 404)
+
+if __name__ == '__main__':
+    program = argv[0]
+    if len(argv) < 3:
+        log(f'usage: {argv[0]} 0.0.0.0 6060')
+        exit(1)
+
+    _, host, port = argv
+    #try:
+    timer()
+    app.jinja_env.auto_reload = True
+    app.config['TEMPLATES_AUTO_RELOAD'] = True
+    app.run(debug=True, host=host, port=int(port), threaded=True)
+    #except Exception as e:
+    #    log(traceback.print_tb(e.__traceback__))
+    #    exit(1)
+
